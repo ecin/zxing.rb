25a372b1626b095fb04ea1dc989381f076fcc196
Joshua Davey <josh@joshuadavey.com>
2013-06-24 21:15:08 -0500

    Allow injection of ZXING_PORT env variable
    This allows reusing a dRB server across multiple client processes.

--- lib/zxing/client.rb
+++ lib/zxing/client.rb
@@ -6,17 +6,19 @@ module ZXing
 
   class Client
     def self.new
-      port = find_available_port
-      remote_client = IO.popen("#{ZXing::BIN} #{port}")
-
-      sleep 0.5 until responsive?(port)
-
-      at_exit { Process.kill(:INT, remote_client.pid) }
-      client = DRbObject.new_with_uri("druby://127.0.0.1:#{port}")
-      client
+      port = ENV['ZXING_PORT'] || find_available_port
+      setup_drb_server(port) unless ENV['ZXING_PORT'] && responsive?(port)
+      DRbObject.new_with_uri("druby://127.0.0.1:#{port}")
     end
 
     private
+
+    def self.setup_drb_server(port)
+      remote_client = IO.popen("#{ZXing::BIN} #{port}")
+      sleep 0.5 until responsive?(port)
+      at_exit { Process.kill(:INT, remote_client.pid) }
+    end
+
     def self.responsive?(port)
       socket = TCPSocket.open('127.0.0.1', port)
       true
